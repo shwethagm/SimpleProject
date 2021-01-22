d77cd869cf113817ebf5d077942da0d6903d3a60
shwethagm <shwethagm9999@gmail.com>
2021-01-22 20:50:34 +0530

    Initial commit

---
+++ src/test/java/Practice1.java
@@ -0,0 +1,15 @@
+class Practice {
+
+    public static void main(String []args){
+        //1 program
+        System.out.println("Hello World");
+        //2 program
+        // System.out.println(("Ada Lovelace"));
+        //3 program
+        System.out.println(("Once upon a time"));
+        System.out.println(("there was"));
+        System.out.println("a program");
+        // 4 program
+        System.out.println(" a dinosaur");
+    }
+}
