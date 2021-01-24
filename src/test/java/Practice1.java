cae0c90b47cc726353cdfa51b72b62996f615341
shwethagm <shwethagm9999@gmail.com>
2021-01-24 17:34:03 +0530

    Adding part1 of 3 chapter

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Practice {
 
-    public static void main(String []args){
+    public  void practice() {
         //1 program
         System.out.println("Hello World");
         //part2 program
@@ -15,28 +15,56 @@ class Practice {
         System.out.println(" a dinosaur");
         //part3-Printing
 
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         //System.out.println("enter the string to be entered");
-        String str=scanner.nextLine();
+        String str = scanner.nextLine();
         System.out.println(str);
 
         //program2
 
-        Scanner scanner1=new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         System.out.println("write your message");
-        String messa=scanner1.nextLine();
+        String messa = scanner1.nextLine();
         System.out.println(messa);
         System.out.println(messa);
         System.out.println(messa);
 
         //program3
-        Scanner scanner3=new Scanner (System.in);
+        Scanner scanner3 = new Scanner(System.in);
         System.out.println("What is your Name");
-        String name=scanner3.nextLine();
-        System.out.println("Hi"+" "+name);
-
-        //program4
+        String name = scanner3.nextLine();
+        System.out.println("Hi" + " " + name);
 
 
     }
+        //program4
+          public void messageReader(){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Greetings! How are you doing?");
+            String messageone = input.nextLine();
+            System.out.println("Oh, how interesting. Tell me more!");
+            String messagetwo = input.nextLine();
+
+            System.out.println("Thanks for sharing!\n");
+            String messagethree = input.nextLine();
+
+        }
+
+public void story(){
+        Scanner input=new Scanner(System.in);
+    System.out.println("I will tell you a story, but I need some information first.\n" +
+            "What is the main character called?");
+    String name=input.nextLine();
+    System.out.println("What is their job?");
+    String job =input.nextLine();
+    System.out.println("Here is the story:\n" +
+            "Once upon a time there was Bob, who was a builder."+"\n" +
+            "On the way to work,"+name+" reflected on life." +"\n"+
+            "Perhaps "+name+" will not be a "+"" +
+            job+" forever."+"\n");
+
 }
+
+//chapter4-Variables
+
+    }
---
+++ src/test/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[]args){
+
+        Practice practice=new Practice();
+       // practice.messageReader();
+        practice.story();
+
+
+    }
+
+}
