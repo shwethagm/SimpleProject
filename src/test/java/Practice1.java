6b97298b92a888b3d16fab89081314cb71d7339b
shwethagm <shwethagm9999@gmail.com>
2021-01-24 17:07:17 +0530

    Adding part1 of 3 chapter

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 class Practice {
 
     public static void main(String []args){
         //1 program
         System.out.println("Hello World");
-        //2 program
+        //part2 program
         // System.out.println(("Ada Lovelace"));
         //3 program
         System.out.println(("Once upon a time"));
@@ -11,5 +13,30 @@ class Practice {
         System.out.println("a program");
         // 4 program
         System.out.println(" a dinosaur");
+        //part3-Printing
+
+        Scanner scanner=new Scanner(System.in);
+        //System.out.println("enter the string to be entered");
+        String str=scanner.nextLine();
+        System.out.println(str);
+
+        //program2
+
+        Scanner scanner1=new Scanner(System.in);
+        System.out.println("write your message");
+        String messa=scanner1.nextLine();
+        System.out.println(messa);
+        System.out.println(messa);
+        System.out.println(messa);
+
+        //program3
+        Scanner scanner3=new Scanner (System.in);
+        System.out.println("What is your Name");
+        String name=scanner3.nextLine();
+        System.out.println("Hi"+" "+name);
+
+        //program4
+
+
     }
 }
